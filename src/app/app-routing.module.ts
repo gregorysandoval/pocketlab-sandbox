bc60621f1c80c13a86c92aa5d32720603a2c8e18
Gregory Sandoval <gregory.sandoval@gmail.com>
2023-04-15 13:23:32 -0700

    Route to launch component on startup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LaunchComponent } from './components/launch/launch.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/launch', pathMatch: 'full' },
+  { path: 'launch', component: LaunchComponent },
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule {
 
---
+++ src/app/components/launch/launch.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-launch',
+  templateUrl: './launch.component.html',
+  styleUrls: ['launch.component.scss']
+})
+export class LaunchComponent {
+
+  constructor() {
+    console.log('init');
+  }
+
+}
